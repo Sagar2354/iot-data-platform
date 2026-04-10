47ddad8eec7d1e64240dd85849027e584d5ee87d
VS Vidyasagr <vidyasagar2354@gmail.com>
2026-04-10 23:17:04 +0530

    Adding sensor data

--- data_generator/generate_iot_data.py
+++ data_generator/generate_iot_data.py
@@ -1,14 +1,14 @@
 import json
 import random
 import time
-from datetime import datetime
+from datetime import datetime, timezone
 
 devices = [f"sensor_{i}" for i in range(1, 11)]
 
 def generate_record():
     return {
         "device_id": random.choice(devices),
-        "timestamp": datetime.utcnow().isoformat(),
+        "timestamp": datetime.now(timezone.utc).isoformat(),
         "temperature": round(random.uniform(20, 100), 2),
         "humidity": round(random.uniform(30, 90), 2),
         "pressure": round(random.uniform(90, 110), 2),
@@ -19,4 +19,6 @@ while True:
     with open("../sample_data/iot_data.json", "a") as f:
         json.dump(generate_record(), f)
         f.write("\n")
+    
+    print("Generated one record...", flush=True)
     time.sleep(1)
